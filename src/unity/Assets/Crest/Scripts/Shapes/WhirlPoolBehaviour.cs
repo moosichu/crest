0e97ba67bdcd4f42ed791aee4297d63c86dd7c54
Tom Maenan Read Cutting <tr395@cam.ac.uk>
2018-06-25 11:20:54 +0100

    Add some test code for whirlpools

---
+++ src/unity/Assets/Crest/Scripts/Shapes/WhirlPoolBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhirlPoolBehaviour : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+    private MeshRenderer mf = null;
+
+	// Use this for initialization
+	void Start () {
+        mf = GetComponent< MeshRenderer > ();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // mf.material.SetFloat("_Time", 10.0f);
+	}
+}
