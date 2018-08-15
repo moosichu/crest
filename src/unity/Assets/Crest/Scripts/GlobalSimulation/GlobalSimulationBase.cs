634687797698b05d1ca84d387f7a3f80b3f09361
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2018-08-15 16:57:46 +0100

    Create skeleton for global simulation base class

---
+++ src/unity/Assets/Crest/Scripts/GlobalSimulation/GlobalSimulationBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalSimulationBase : MonoBehaviour {
+
+	// TODO: We want two independent variables
+	// - The detail level of the simulation (as-in how detailed it is)
+	// - The LOD level of the simulation (as-in, up to which LOD it is displayed at)
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
