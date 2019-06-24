6eade2446bd7545927c40e310cfb28c25a5862dc
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2019-06-24 08:31:31 +0100

    Create initial post-process shader for underwater effect

---
+++ crest/Assets/Crest/Crest/Scripts/Helpers/UnderwaterPostProcess.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderwaterPostProcess : MonoBehaviour
+{
+    public Material _underWaterPostProcMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture target)
+    {
+        Graphics.Blit(source, target, _underWaterPostProcMat);
+    }
+}
