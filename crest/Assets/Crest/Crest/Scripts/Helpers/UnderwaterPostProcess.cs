8d955be9f52aee998695dff589d6e93edd679bc1
Tom Maenan Read Cutting <tr395@cam.ac.uk>
2019-07-08 21:48:02 +0100

    Make horizon calculation explicit

--- crest/Assets/Crest/Crest/Scripts/Helpers/UnderwaterPostProcess.cs
+++ crest/Assets/Crest/Crest/Scripts/Helpers/UnderwaterPostProcess.cs
@@ -31,34 +31,15 @@ namespace Crest
 
             // //horizonHeight = oceanTransform.position.y - _mainCamera.transform.position.y;
 
-            float fieldOfView = _mainCamera.fieldOfView;
+            float halfFov = _mainCamera.fieldOfView * 0.5f;
             Vector3 cameraForward = _mainCamera.transform.forward;
             // TODO(UPP): handle Roll
-            // float cameraRotation = Mathf.Atan2(-1.0f * cameraForward.y, cameraForward.x);
-            // horizonHeight = 0.5f + cameraRotation / (Mathf.Deg2Rad * fieldOfView);
-
-            // float tanCameraRotation = cameraForward.y * (new Vector2(cameraForward.x, cameraForward.z)).magnitude;
-            // horizonHeight = tanCameraRotation / Mathf.Tan(Mathf.Deg2Rad * fieldOfView);
-            // horizonHeight = (horizonHeight * -2.0f) + 0.5f;
-
             float cameraRotation = Mathf.Atan2(-1.0f * cameraForward.y, (new Vector2(cameraForward.x, cameraForward.z)).magnitude);
-
-            float halfProp = Mathf.Tan(cameraRotation * 0.5f) / Mathf.Tan(fieldOfView * Mathf.Deg2Rad * 0.5f);
-
+            float halfProp = Mathf.Tan(cameraRotation * 0.5f) / Mathf.Tan(halfFov * Mathf.Deg2Rad);
             horizonHeight = halfProp + 0.5f;
-
-            // Matrix4x4 cameraMatrix = _mainCamera.projectionMatrix * _mainCamera.worldToCameraMatrix;
-            // Vector3 planeNormalCS = cameraMatrix.MultiplyVector(Vector3.up);
-            // horizonHeight = 0.5f - planeNormalCS.z;// * (planeNormalCS.y / planeNormalCS.magnitude);
-            // horizonHeight = Mathf.Asin(horizonHeight);
-
-            Debug.Log(horizonHeight);
-            // Debug.Log(new Vector2(horizonHeight, h2));
 
             _underWaterPostProcMat.SetFloat(sp_HorizonHeight, horizonHeight);
             _underWaterPostProcMat.SetFloat(sp_HorizonOrientation, horizonRoll);
-
-
             // TOOD(UPP): Disable post-proc iff we don't have water intersecting the camera.
             Graphics.Blit(source, target, _underWaterPostProcMat);
         }
