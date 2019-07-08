5a59359da75f6d03b8f586a1cd6fe844c18c8a75
Tom Maenan Read Cutting <tr395@cam.ac.uk>
2019-07-08 16:02:20 +0100

    Improve horizon tracking for post-proc input

--- crest/Assets/Crest/Crest/Scripts/Helpers/UnderwaterPostProcess.cs
+++ crest/Assets/Crest/Crest/Scripts/Helpers/UnderwaterPostProcess.cs
@@ -30,10 +30,18 @@ namespace Crest
 
             Transform oceanTransform = OceanRenderer.Instance.transform;
 
-            horizonHeight = oceanTransform.position.y - _mainCamera.transform.position.y;
+            //horizonHeight = oceanTransform.position.y - _mainCamera.transform.position.y;
+
+            float fieldOfView = _mainCamera.fieldOfView;
+            Vector3 cameraForward = _mainCamera.transform.forward;
+            // TODO(UPP): handle orientations
+            float cameraRotaion = Mathf.Atan2(-1.0f * cameraForward.y, cameraForward.x);
+            horizonHeight = 0.5f + cameraRotaion / (Mathf.Deg2Rad * fieldOfView);
 
             _underWaterPostProcMat.SetFloat(sp_HorizonHeight, horizonHeight);
             _underWaterPostProcMat.SetFloat(sp_HorizonOrientation, horizonOrientation);
+
+            // TOOD(UPP): Disable post-proc iff we don't have water intersecting the camera.
             Graphics.Blit(source, target, _underWaterPostProcMat);
         }
     }
